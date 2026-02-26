d0ddf2450416ef4c220bce1a4c5144f17b0b3a91
Sid4030 <chosiddhant125@gmail.com>
2026-02-26 20:12:32 +0530

    error fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
+  build: {
+    rollupOptions: {
+      external: [
+        "https://puter-net.b-cdn.net/rustls.js",
+        "https://unpkg.com/web-streams-polyfill@3.0.2/dist/polyfill.js"
+      ]
+    }
+  }
 });
